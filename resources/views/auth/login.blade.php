46c52186dbd3039661159edea4e34e8733c69892
alexandresolymos <51198792+alexandre427@users.noreply.github.com>
2021-05-20 17:34:40 +0200

    Modification style login + register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,7 +53,7 @@
 
                 <div class="form-group">
                     <div class="col-md-8 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
+                        <button type="submit" class="btn-primary">
                             {{ __('Login') }}
                         </button>
 
