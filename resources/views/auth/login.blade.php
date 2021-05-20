a4a8607e04f6ede8b5870bb28c498a96b7f706e9
alexandresolymos <51198792+alexandre427@users.noreply.github.com>
2021-05-20 17:15:35 +0200

    Creation page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,15 +4,15 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-
-            <div class="card-header">{{ __('Login') }}</div>
+            <div class="border-shadow">
+                <div class="card-header">{{ __('Login') }}</div>
 
 
             <form method="POST" action="{{ route('login') }}">
                 @csrf
 
-                <div class="form-group row">
-                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                <div class="form-group">
+                    <label for="email" >{{ __('E-Mail Address') }}</label>
 
                     <div class="col-md-6">
                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -25,7 +25,7 @@
                     </div>
                 </div>
 
-                <div class="form-group row">
+                <div class="form-group">
                     <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                     <div class="col-md-6">
@@ -39,7 +39,7 @@
                     </div>
                 </div>
 
-                <div class="form-group row">
+                <div class="form-remember">
                     <div class="col-md-6 offset-md-4">
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -51,7 +51,7 @@
                     </div>
                 </div>
 
-                <div class="form-group row mb-0">
+                <div class="form-group">
                     <div class="col-md-8 offset-md-4">
                         <button type="submit" class="btn btn-primary">
                             {{ __('Login') }}
@@ -65,7 +65,7 @@
                     </div>
                 </div>
             </form>
-
+            </div>
         </div>
     </div>
 
