949601aa702d0f2dd1ea3bfa3624562ddbe4558d
alexandresolymos <51198792+alexandre427@users.noreply.github.com>
2021-05-25 21:51:44 +0200

    Fixt: intégration Respi CSS

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,26 +6,41 @@
 
 
 @section('content')
-    <div class="jumbotron">
-        <h1 class="display-4">{{ $product->title }}</h1>
-        <p class="lead">  <p>{{ $product->subtitle }}</p>
-        <hr class="my-4">
-        <img src="{{ asset('/img/'. $product->image) }}" alt="{{ $product->balise_alt }}">
 
-        <p>Description : {{ $product->description }}</p>
-        <p>Categorie : {{$category->title}}</p>
-        <p>Prix : {{ $product->price }}</p>
-        <p class="lead">
-        <form action="{{ route('cart.store') }}" method="POST">
-            @csrf
-            <input type="hidden" name="id" value="{{ $product->id }}">
-            <input type="hidden" name="title" value="{{ $product->title }}">
-            <input type="hidden" name="subtitle" value="{{ $product->subtitle }}">
-            <input type="hidden" name="price" value="{{ $product->price }}">
-            <button type="submit" id="addcart">Ajouter au panier</button>
-        </form>
-        </p>
-    </div>
+   <div class="container-all">
+        <div class="row">
+
+            <div class="col-6-t">
+                <div class="block-left-image">
+                    <img width="100%" src="{{ asset('/img/'. $product->image) }}" alt="{{ $product->balise_alt }}">
+                </div>
+            </div>
+
+
+            <div class="col-6-t">
+                <div class="block-right-other">
+                    <h1 class="display-4">{{ $product->title }}</h1>
+                    <p class="lead">  <p>{{ $product->subtitle }}</p>
+                    <hr class="my-4">
+
+                    <p>Description : {{ $product->description }}</p>
+                    <p>Categorie : {{$category->title}}</p>
+                    <p>Prix : {{ $product->price }}</p>
+
+                    <form action="{{ route('cart.store') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{ $product->id }}">
+                        <input type="hidden" name="title" value="{{ $product->title }}">
+                        <input type="hidden" name="subtitle" value="{{ $product->subtitle }}">
+                        <input type="hidden" name="price" value="{{ $product->price }}">
+                        <button type="submit" id="addcart">Ajouter au panier</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+   </div>
+
 @stop
 
 
